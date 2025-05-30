0c57870f03dfb4fb38ca94c58fce92909c00aecd
GoldFisher20 <gikkoyplay@gmail.com>
2025-05-30 14:23:02 +0300

    with db connected

--- DailyBoost/Program.cs
+++ DailyBoost/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+string dbConnectionString = builder.Configuration.GetConnectionString("Default");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
